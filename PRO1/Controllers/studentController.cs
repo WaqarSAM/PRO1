db2896fedde778166fc61d0e66679d14d756d3c1
waqar <waqar@DESKTOP-80TA7ML>
2018-09-21 15:28:05 +0500

    Detail view and Allstudents

--- PRO1/Controllers/studentController.cs
+++ PRO1/Controllers/studentController.cs
@@ -30,6 +30,28 @@ namespace PRO1.Controllers
             ModelState.Clear();
             return View();
         }
+        public IActionResult StudentDetail(int Id)
+        {
+            Student S = ORM.Student.Where(m => m.Id == Id).FirstOrDefault<Student>();
+            return View(S);
+        }
+
+
+
+        [HttpGet]
+        public IActionResult AllStudents()
+        {
+            IList<Student> AllStudents = ORM.Student.ToList<Student>();
+            return View(AllStudents);
+        }
+
+        [HttpPost]
+        public IActionResult AllStudents(string SearchByName, string SearchByClass)
+        {
+
+            IList<Student> AllStudents = ORM.Student.Where(m => m.Name.Contains(SearchByName) || m.Class.Contains(SearchByName)).ToList<Student>();
+            return View(AllStudents);
+        }
 
 
 
