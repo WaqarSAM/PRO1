1af60e1804b73054bb3aa8b141403192802faeda
waqar <waqar@DESKTOP-80TA7ML>
2018-09-21 13:17:04 +0500

    Project with student controller

---
+++ PRO1/Controllers/studentController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PRO1.Controllers
+{
+    public class studentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
